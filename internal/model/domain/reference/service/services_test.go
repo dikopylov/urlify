1b3529732a88302d2311db7e202c5c5aedf77612
Dmitry Kopylov <dikopylov10@gmail.com>
2022-01-24 18:43:43 +0500

    Added Service tests

--- internal/model/domain/reference/service/services_test.go
+++ internal/model/domain/reference/service/services_test.go
@@ -80,3 +80,66 @@ func TestReferenceService_Encode_ReferenceDoesntExists_FailInsert(t *testing.T) 
 	repositoryMock.AssertExpectations(t)
 	factoryMock.AssertExpectations(t)
 }
+
+func TestReferenceService_Encode_ReferenceAlreadyExists(t *testing.T) {
+	repositoryMock := new(RepositoryMock)
+	factoryMock := new(FactoryMock)
+
+	link := "https://test.com"
+	reference := &model.Reference{}
+	criteria := repository.Criteria{}
+	criteria.AddParameter(repository.ColumnUrl, link)
+
+	repositoryMock.On("GetByCriteria", criteria).Return(reference, nil)
+
+	service := NewReferenceService(repositoryMock, factoryMock)
+	actual, actualErr := service.Encode(link)
+	expected := reference
+
+	assert.Equal(t, actual, expected)
+	assert.Nil(t, actualErr)
+	repositoryMock.AssertExpectations(t)
+	factoryMock.AssertExpectations(t)
+}
+
+func TestReferenceService_Encode_ReferenceDoesntExistsReturnCustomError(t *testing.T) {
+	repositoryMock := new(RepositoryMock)
+	factoryMock := new(FactoryMock)
+
+	link := "https://test.com"
+	var nilReference *model.Reference
+	criteria := repository.Criteria{}
+	criteria.AddParameter(repository.ColumnUrl, link)
+
+	repositoryMock.On("GetByCriteria", criteria).Return(nilReference, sql.ErrTxDone)
+
+	service := NewReferenceService(repositoryMock, factoryMock)
+	actual, actualErr := service.Encode(link)
+	expectedErr := sql.ErrTxDone
+
+	assert.Nil(t, actual)
+	assert.Equal(t, actualErr, expectedErr)
+	repositoryMock.AssertExpectations(t)
+	factoryMock.AssertExpectations(t)
+}
+
+func TestReferenceService_GetByCriteria_CallRepositoryMethod(t *testing.T) {
+	repositoryMock := new(RepositoryMock)
+	factoryMock := new(FactoryMock)
+
+	link := "https://test.com"
+	newReference := &model.Reference{}
+	criteria := repository.Criteria{}
+	criteria.AddParameter(repository.ColumnUrl, link)
+
+	repositoryMock.On("GetByCriteria", criteria).Return(newReference, nil)
+
+	service := NewReferenceService(repositoryMock, factoryMock)
+	actual, actualErr := service.GetByCriteria(criteria)
+	expected := newReference
+
+	assert.Equal(t, actual, expected)
+	assert.Nil(t, actualErr)
+	repositoryMock.AssertExpectations(t)
+	factoryMock.AssertExpectations(t)
+}
